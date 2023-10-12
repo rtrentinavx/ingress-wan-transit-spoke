3cf1323a93a01ca1656bcbfc67cddefd996c32a3
Ricardo Trentin <rtrentin@aviatrix.com>
2023-10-12 17:30:28 -0400

    sdwan forti

--- terraform/sdwan/provider.tf
+++ terraform/sdwan/provider.tf
@@ -5,6 +5,12 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "3.75.0"
     }
+    random = {
+      source = "hashicorp/random"
+    }
+    template = {
+      source = "hashicorp/template"
+    }
   }
     backend "azurerm" {
     resource_group_name  = ""
@@ -17,4 +23,9 @@ terraform {
 
 provider "azurerm" {
   features {}
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
