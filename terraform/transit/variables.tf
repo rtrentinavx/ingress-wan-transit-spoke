09693d56dfcade66181d3a00658b1fecfa13d405
Ricardo Trentin <rtrentin@aviatrix.com>
2023-10-17 09:30:36 -0400

    backend

--- terraform/transit/variables.tf
+++ terraform/transit/variables.tf
@@ -3,7 +3,6 @@ variable "client_id" { type = string }
 variable "client_secret_file_path" { type = string }
 variable "tenant_id" { type = string }
 variable "location" { type = string }
-
 variable "resource_group_name" { type = string }
 variable "storage_account_name" { type = string }
 variable "container_name" { type = string }
