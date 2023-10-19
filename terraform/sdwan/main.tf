550f3427c7dd79f5db7cdd432fd11678c97de3c8
Ricardo Trentin <rtrentin@aviatrix.com>
2023-10-19 17:22:58 -0400

    sdwan templatefile

--- terraform/sdwan/main.tf
+++ terraform/sdwan/main.tf
@@ -21,7 +21,7 @@ resource "azurerm_route" "default" {
   depends_on             = [azurerm_virtual_machine.passivefgtvm]
   name                   = "default"
   resource_group_name    = data.azurerm_resource_group.resource-group.name
-  route_table_name       = data.azurerm_route_table.internal.name
+  route_table_name       = azurerm_route_table.internal.name
   address_prefix         = "0.0.0.0/0"
   next_hop_type          = "VirtualAppliance"
   next_hop_in_ip_address = var.activeport3
